e523a636648a9573f128597a02d25e2bb5432d02
WoodenScout <leonardotoffalini@gmail.com>
2023-11-27 13:00:56 +0100

    adds single player mode

--- main.py
+++ main.py
@@ -4,33 +4,61 @@ from game import Game
 
 
 def main():
+    palyers = int(input('1 or 2 players? '))
     size = (600, 600)
     screen = pygame.display.set_mode(size)
     board = Board(screen, size[0], size[1], 'X')
     game = Game(board)
 
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+
+    if palyers == 2:
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
+                if event.type == pygame.MOUSEBUTTONUP:
+                    pos = pygame.mouse.get_pos()
+                    board.handle_click(*pos)
+                if event.type == pygame.KEYDOWN:
+                    print(game.minimax(player = board.turn, depth = 9))
+                    print(game.alphabeta(player = board.turn, depth = 9))
+
+            if game.check_winner() is not None:
+                if (winner := game.check_winner()) in ['O', 'X']:
+                    print(f'Winner: {winner}')
+                else:
+                    print('Tie')
                 running = False
 
-            if event.type == pygame.MOUSEBUTTONUP:
-                pos = pygame.mouse.get_pos()
-                board.handle_click(*pos)
-            if event.type == pygame.KEYDOWN:
-                print(game.minimax())
-                print(game.alphabeta())
+            board.draw()
+            pygame.display.update()
+    elif palyers == 1:
+        running = True
+        while running:
+            if board.turn == 'X':
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        running = False
 
-        if game.check_winner() is not None:
-            if (winner := game.check_winner()) in ['O', 'X']:
-                print(f'Winner: {winner}')
+                    if event.type == pygame.MOUSEBUTTONUP:
+                        pos = pygame.mouse.get_pos()
+                        board.handle_click(*pos)
             else:
-                print('Tie')
-            running = False
+                move = game.alphabeta(player = 'O', depth = 9)
+                game.make_move(move[0], move[1], 'O')
+                board.turn = 'X'
 
-        board.draw()
-        pygame.display.update()
+            if game.check_winner() is not None:
+                if (winner := game.check_winner()) in ['O', 'X']:
+                    print(f'Winner: {winner}')
+                else:
+                    print('Tie')
+                running = False
+
+            board.draw()
+            pygame.display.update()
 
 if __name__ == '__main__':
     main()
